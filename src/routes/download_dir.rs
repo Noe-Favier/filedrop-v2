94faee60ae7616e16afa9b113f412713b1d2ddbd
Noé Favier <noe.favier@outlook.com>
2023-01-07 17:17:44 +0100

    fix(folder support): tempfile lifecycle

--- src/routes/download_dir.rs
+++ src/routes/download_dir.rs
@@ -2,7 +2,7 @@ use std::{fs::File, path::Path};
 use rocket::{response::content};
 use rocket::response::status::NotFound;
 use std::env;
-use tempfile::tempfile;
+use tempfile::{tempfile, NamedTempFile};
 
 use crate::utils::dir_zipper::zip_dir;
 
@@ -17,10 +17,10 @@ pub fn download_dir(name: &str, _formatted_name: &str) -> Result<content::RawMsg
     }
 
     //**************** TEMP FILE CREATION ****************//
-    let tempfile = tempfile().expect("can't create empty temp file");
+    let tempfile = NamedTempFile::new().expect("can't create empty temp file");
 
-    println!("added file to {:?}", &tempfile);
+    println!("added file to {:?}", &tempfile.as_file());
 
-    zip_dir(path_to_dir, &tempfile).expect("can't zip");
-    return Ok(content::RawMsgPack(Some(tempfile)));
+    zip_dir(path_to_dir, tempfile.as_file()).expect("can't zip");
+    return Ok(content::RawMsgPack(Some(tempfile.reopen().expect("can't reopen the temp file"))));
 }
