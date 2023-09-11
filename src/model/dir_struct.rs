be2926d41d468ede5ac6aabf37d21b91b555c558
Noé Favier <noe.favier@outlook.com>
2023-09-11 21:28:39 +0200

    fix(folders): wrong file count in case of multiple nested subfolders

--- src/model/dir_struct.rs
+++ src/model/dir_struct.rs
@@ -1,9 +1,5 @@
 use chrono::prelude::{DateTime, Local};
-use std::{
-    fs::{read_dir},
-    path::{PathBuf},
-    time::SystemTime,
-};
+use std::{env, fs::read_dir, path::PathBuf, time::SystemTime};
 
 use super::file_struct::FileDropFile;
 
@@ -27,7 +23,7 @@ impl FileDropDir {
             name: name,
             size: self::FileDropDir::get_total_size(&path_to_dir),
             date_last_modified: format!("{}", dt.format("%v - %T")),
-            files: self::FileDropDir::get_file_list(&path_to_dir),
+            files: self::FileDropDir::get_file_list(&path_to_dir, false),
         };
     }
 
@@ -47,27 +43,51 @@ impl FileDropDir {
         return temp_count;
     }
 
-    pub fn get_file_list(path: &PathBuf) -> Vec<FileDropFile> {
+    pub fn get_file_list(path: &PathBuf, sub: bool) -> Vec<FileDropFile> {
+        //sub is used to mark file with full path
         let mut temp_list: Vec<FileDropFile> = Vec::new();
 
         let entries = read_dir(path);
 
         if entries.is_ok() {
             for f in entries.unwrap() {
-                if f.is_ok() {
-                    let file = f.unwrap();
-                    temp_list.push(FileDropFile::new(
-                        file.file_name()
+                if f.is_err() {
+                    continue;
+                }
+                let f = f.unwrap();
+                if f.path().is_file() {
+                    let file = f;
+
+                    let filename: String;
+                    if sub {
+                        filename = file
+                            .path()
+                            .to_str()
+                            .unwrap_or("/invalid sub file name/")
+                            .to_string()
+                            .replace(
+                                &env::var("files_path").unwrap_or(String::from("./files")),
+                                ".",
+                            );
+                    } else {
+                        filename = file
+                            .file_name()
                             .to_str()
                             .ok_or("/invalid filename/")
                             .unwrap()
-                            .to_string(),
+                            .to_string();
+                    }
+
+                    temp_list.push(FileDropFile::new(
+                        filename,
                         mime_guess::from_path(file.path())
                             .first_or_octet_stream()
                             .to_string(),
                         file.metadata().unwrap().len(),
                         file.metadata().unwrap().modified().unwrap(),
                     ));
+                } else {
+                    temp_list.append(&mut Self::get_file_list(&f.path(), true));
                 }
             }
         }
